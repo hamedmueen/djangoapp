8181c5325330b33fe475b15ac294178e04ab4b3f
hamedmueen <64982261+hamedmueen@users.noreply.github.com>
2022-07-01 17:45:18 -0400

    Delete djangoApp directory

--- djangoApp/open/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class OpenConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'open'
--- djangoApp/open/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.openPage, name='openPage'),
-    path('register', views.registerPage, name='registerPage'),
-    path('login', views.loginPage, name='loginPage')
-]
